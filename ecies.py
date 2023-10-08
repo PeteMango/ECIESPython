22b8ac9abb0e9334ef9e796353e60327af5587d4
Peter Wang <40186116+PeteMango@users.noreply.github.com>
2023-10-08 17:11:32 -0400

    Update ecies.py

--- ecies.py
+++ ecies.py
@@ -1,87 +1,82 @@
-import os, binascii
-from cryptography.hazmat.primitives import hashes, hmac
-from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+import os, sys, struct
+from cryptography.hazmat.primitives import hashes, hmac, serialization, keywrap
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric import ec, rsa, padding
+from cryptography.hazmat.primitives.kdf.x963kdf import X963KDF
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 
 """
-constants
+encrypt ()
+params: remote public key object, plaintext (to be encrypted)
+returns: ciphertext (local public key, ciphertext, mac tag)
 """
-curve = ec.SECP384R1()
-plaintext = b'a secret message'
-encryption_key_length = 32
-random_number = os.urandom(16)
-shared_info_1 = b'Peter and Harry'
-shared_info_2 = b'Harry and Peter'
-hash_algorithm = hashes.SHA256()
-field_length = int(curve.key_size / 8)
-hash_length = hash_algorithm.digest_size
+def ecies_encrypt (remote_public_key, plaintext):
+    # curve (supports 256, 384 and 512 but currently hardcoded to 256)
+    # hash algorithm (css supports sha-256-128)
+    curve = ec.SECP256R1()
+    hash_algorithm = hashes.SHA256()
 
-def private_key_generate (curve):
-    return ec.generate_private_key(curve)
+    # derived constants from variables specified above
+    encryption_key_length = len(plaintext)
+    field_length = int(curve.key_size / 8)
+    hash_length = hash_algorithm.digest_size
+    mac_length =  int(hash_length / 2)
 
-def shared_key_generate (private_key, public_key):
-    return private_key.exchange(ec.ECDH(), public_key)
+    # generate private key from specified elliptical curve
+    # derive key using diffie hellman key exchange
+    prv_key = ec.generate_private_key(curve)
+    drv_key = X963KDF(hash_algorithm, encryption_key_length+hash_length, None).derive(prv_key.exchange(ec.ECDH(), remote_public_key))
 
-def derived_key_generate (shared_key, shared_info_1, encryption_key_length, hash_length):
-    derived_key = HKDF(algorithm=hash_algorithm, length=encryption_key_length+hash_length, salt=None, info=shared_info_1).derive(shared_key)
-    return (derived_key, derived_key[:encryption_key_length], derived_key[encryption_key_length:])
-
-def encrypt_plaintext (encryption_key, plaintext, random_number):
-    encryptor = Cipher(algorithms.AES(encryption_key), modes.CBC(random_number)).encryptor()
-    return (encryptor.update(plaintext) + encryptor.finalize())
-
-def decrypt_ciphertext (encryption_key, ciphertext, random_number):
-    decryptor = Cipher(algorithms.AES(encryption_key), modes.CBC(random_number)).decryptor()
-    return (decryptor.update(ciphertext) + decryptor.finalize())
-
-def mac (mac_key, ciphertext, shared_info_2):
+    # derived mac and encryption key and encrypt the ciphertext (xor) 
+    mac_key, enc_key = drv_key[:hash_length], drv_key[hash_length:]
+    enc_plaintext = bytes(a ^ b for (a, b) in zip (enc_key, plaintext))
     mac = hmac.HMAC(mac_key, hash_algorithm)
-    mac.update(ciphertext + shared_info_2)
-    return (mac, mac.finalize())
+    mac.update(enc_plaintext)
+    mac_digest = mac.finalize()[:mac_length]
 
-def encapsulation (private_key, ciphertext, digest):
-    return private_key.public_key().public_bytes(Encoding.X962, PublicFormat.UncompressedPoint) + ciphertext + digest
+    # return the ciphertext
+    return (prv_key.public_key().public_bytes(Encoding.X962, PublicFormat.UncompressedPoint) + enc_plaintext + mac_digest)
 
-def point_type_check (code, field_length):
-    if code == 2 or code == 3:
-        return field_length + 1 # compressed (the x-coordinate is a single bit)
-    elif code == 4:
-        return 2 * field_length + 1 # uncompressed
-    else:
-        raise TypeError('Invalid Public Key Format')
+
+"""
+decrypt ()
+params: keyblob, local private key object
+returns: decrypted plaintext
+"""
+def ecies_decrypt (keyblob, prv_key):
+    # curve (supports 256, 384 and 512 but currently hardcoded to 256)
+    # hash algorithm (css supports sha-256-128)
+    curve = ec.SECP256R1()
+    hash_algorithm = hashes.SHA256()
+
+    # derived constants from the variables above
+    field_length = int(curve.key_size / 8)
+    hash_length = hash_algorithm.digest_size
+    mac_length =  int(hash_length / 2)
     
-def public_key_compute (curve, point_length):
-    return ec.EllipticCurvePublicKey.from_encoded_point(curve, output[:point_length])
+    # check whether the public key is compressed or not
+    if keyblob[0] == 4:
+        point_length = 2 * field_length + 1
+    else:
+        raise TypeError("Does not support compressed keys")
 
-def output_decomposition (output, point_length, hash_length):
-    return (output[point_length:len(output) - hash_length], output[-hash_length:])
+    # derive the remote public key from the keyblob
+    remote_public_key = ec.EllipticCurvePublicKey.from_encoded_point(curve, keyblob[:point_length])
+    ciphertext =  keyblob[point_length:len(keyblob) - mac_length]
+    digest = keyblob[-mac_length:]
+    encryption_key_length = len(ciphertext)
+    
+    # derive the derived key, mac and encryption key to decrypt the plaintext
+    drv_key = X963KDF(hash_algorithm, encryption_key_length+hash_length, None).derive(prv_key.exchange(ec.ECDH(), remote_public_key))
+    mac_key, enc_key = drv_key[:hash_length], drv_key[hash_length:]
+    plaintext = bytes(a ^ b for (a, b) in zip (enc_key, ciphertext))
 
-def verify_encryption_decryption (digest, derived_digest, plaintext, derived_plaintext):
-    assert digest == derived_digest
-    assert plaintext == derived_plaintext
+    mac = hmac.HMAC(mac_key, hash_algorithm)
+    mac.update(ciphertext)
+    digest2 = mac.finalize()[:mac_length]
 
-alice_private_key, bob_private_key = private_key_generate(curve), private_key_generate(curve)
+    # assert the digest from the keyblob is equal to the digest from the hmac
+    assert digest == digest2
 
-shared_key = shared_key_generate(alice_private_key, bob_private_key.public_key())
-
-derived_key, encryption_key, mac_key = derived_key_generate(shared_key, shared_info_1, encryption_key_length, hash_length)
-
-encrypted_plaintext = encrypt_plaintext(encryption_key, plaintext, random_number)
-message_authentication_code, digest = mac(mac_key, encrypted_plaintext, shared_info_2)
-
-output = encapsulation(alice_private_key, encrypted_plaintext, digest)
-
-point_length = point_type_check(output[0], field_length) 
-
-alice_public_key = public_key_compute(curve, point_length) # alice_public_key = alice_private_key.public_key()
-derived_encrypted_plaintext, derived_digest = output_decomposition(output, point_length, hash_length) # derived_encrypted_plaintext = encrypted_plaintext
-
-derived_shared_key = shared_key_generate(bob_private_key, alice_public_key) # derived_shared_key = shared_key
-
-decrypted_ciphertext = decrypt_ciphertext(encryption_key, derived_encrypted_plaintext, random_number)
-
-derived_maessage_authentication_code = mac(mac_key, encrypted_plaintext, shared_info_2)
-
-verify_encryption_decryption(digest, derived_digest, plaintext, decrypted_ciphertext)
+    # returns the plaintext
+    return plaintext
